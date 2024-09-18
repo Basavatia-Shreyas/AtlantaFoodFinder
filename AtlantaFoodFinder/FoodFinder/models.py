9693a54f5dc041cbe7c528b839469b1a45d5f0a2
Shreyas Basavatia <shreyasb205@gmail.com>
2024-09-18 16:02:19 -0400

    working blank pages

--- AtlantaFoodFinder/FoodFinder/models.py
+++ AtlantaFoodFinder/FoodFinder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    # Figure out how to store the resturant - list of char, resturaunt objects, etc?
+    #favorites = models
--- AtlantaFoodFinder/FoodFinder/views.py
+++ AtlantaFoodFinder/FoodFinder/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the Food Finder home page.")
+
+def login(request):
+    return render(request, "FoodFinder/login.html")
+
+def create_account(request):
+    return render(request, "FoodFinder/create_account.html")
+
+def restaurant(request):
+    return render(request, "FoodFinder/restaurant.html")
+
+def favorites(request):
+    return render(request, "FoodFinder/favorites.html")
