724ac127025bf114c63aba9fd4aed9263250e7c5
arsheyagourav <arsheya.gourav29@gmail.com>
2024-09-19 13:54:49 -0400

    test

--- AtlantaFoodFinder/FoodFinder/models.py
+++ AtlantaFoodFinder/FoodFinder/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db import modelsR
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine_type = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    rating = models.FloatField()
+    place_id = models.CharField(max_length=255)
