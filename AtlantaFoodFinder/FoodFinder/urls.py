2c0099566526cafd74b4bfdfd97320f88d832527
Shreyas Basavatia <shreyasb205@gmail.com>
2024-09-18 22:44:55 -0400

    auth working

--- AtlantaFoodFinder/FoodFinder/urls.py
+++ AtlantaFoodFinder/FoodFinder/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name="login"),
+    path("", views.loginPage, name="login"),
     path("createaccount/", views.create_account, name="create account"),
     path("home/", views.index, name="index"),
     path("favorites/", views.favorites, name="favorites"),
     path("restaurant/", views.restaurant, name="restaurant"),
+    path("logout/", views.logoutUser, name="logout"),
 ]
--- AtlantaFoodFinder/FoodFinder/views.py
+++ AtlantaFoodFinder/FoodFinder/views.py
@@ -1,32 +1,59 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .templates.forms import CreateUserForm
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the Food Finder home page.")
+    return render(request, "FoodFinder/home.html")
 
-def login(request):
-    return render(request, "FoodFinder/login.html")
+@csrf_protect
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            messages.info(request, "Username or password is incorrect")
+
+    context = {}
+    return render(request, "FoodFinder/login.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect("login")
 
 @csrf_protect
 def create_account(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect("index")
+    else:
+        form = CreateUserForm()
 
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("login")
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect("login")
 
-    context = {"form" : form}
-    return render(request, "FoodFinder/create_account.html", context)
+        context = {"form" : form}
+        return render(request, "FoodFinder/create_account.html", context)
 
 def restaurant(request):
     return render(request, "FoodFinder/restaurant.html")
 
+@login_required(login_url="login")
 def favorites(request):
     return render(request, "FoodFinder/favorites.html")
