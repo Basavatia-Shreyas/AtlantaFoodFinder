2ab8184fa921ddca42ec0d1b183a091137ba11ca
Shreyas Basavatia <shreyasb205@gmail.com>
2024-09-19 14:40:11 -0400

    working api call

--- AtlantaFoodFinder/FoodFinder/views.py
+++ AtlantaFoodFinder/FoodFinder/views.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+import json
+import requests
+
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
@@ -6,12 +15,30 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+import googlemaps
+map_client = googlemaps.Client(api_key)
+
 from .templates.forms import CreateUserForm
 
-
 # Create your views here.
+@csrf_protect
 def index(request):
-    return render(request, "FoodFinder/home.html")
+    find_place = map_client.find_place("Blue India", 'textquery')
+    place_id = find_place["candidates"][0]["place_id"]
+    response = map_client.place(place_id)
+
+    if response["status"] == "OK":
+        print('Successful search!')
+        print(response)  # Print the JSON response from the API
+        context = response
+        #return JsonResponse({'status': 'success', 'data': response})
+    else:
+        print(f"Error: {response['status']}")
+        print(response)
+        context = response
+        #return JsonResponse({'status': 'error', 'message': response}, status=response['status'])
+
+    return render(request, "FoodFinder/home.html", context=context)
 
 @csrf_protect
 def loginPage(request):
