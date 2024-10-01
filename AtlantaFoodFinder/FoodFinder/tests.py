05c343f0666f9ee5dec84b21eb297beaa7e667c8
arsheyagourav <arsheya.gourav29@gmail.com>
2024-09-30 20:41:42 -0400

    tests

--- AtlantaFoodFinder/FoodFinder/tests.py
+++ AtlantaFoodFinder/FoodFinder/tests.py
@@ -1,3 +1,33 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from . import views
 from django.test import TestCase
 
-# Create your tests here.
+class TestUrls(SimpleTestCase):
+    def test_login_url_resolves(self):
+        url = reverse('login')
+        self.assertEqual(resolve(url).func, views.loginPage)
+
+    def test_create_account_url_resolves(self):
+        url = reverse('create account')
+        self.assertEqual(resolve(url).func, views.create_account)
+
+    def test_index_url_resolves(self):
+        url = reverse('index')
+        self.assertEqual(resolve(url).func, views.index)
+
+    def test_favorites_url_resolves(self):
+        url = reverse('favorites')
+        self.assertEqual(resolve(url).func, views.favorites)
+
+    def test_restaurant_url_resolves(self):
+        url = reverse('restaurant')
+        self.assertEqual(resolve(url).func, views.restaurant)
+
+    def test_logout_url_resolves(self):
+        url = reverse('logout')
+        self.assertEqual(resolve(url).func, views.logoutUser)
+
+
+
+
